d102fbcad56a80faef9df94fbbab81594e9a036e
Anya Helene Bagge <anya@ii.uib.no>
2012-10-22 15:16:40 +0000

    Add hoverhelp for local definitions.
    TODO: Code that gets removed or moved around during flattening won't show up in the hover help. Solutions:
    
      - use old index map solution for hover help on typechecked stuff
      
      - look for hoverhelp in the closest enclosing top-level construct (which will be a "{}", "on {} defines {}" or similar)

--- magnolia-eclipse/src/org/magnolialang/resources/IXRefInfo.java
+++ magnolia-eclipse/src/org/magnolialang/resources/IXRefInfo.java
@@ -1,9 +1,18 @@
 package org.magnolialang.resources;
 
 import org.eclipse.imp.pdb.facts.IConstructor;
+import org.eclipse.imp.pdb.facts.ISourceLocation;
 
 public interface IXRefInfo {
 
 	IConstructor getDefInfo();
 
+
+	IConstructor getId();
+
+
+	ISourceLocation getSrcLoc();
+
+
+	boolean hasDefInfo();
 }
